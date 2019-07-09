bca79ee62b6bab2bfdf109b87ee6173f1b0f675f
namickey <teethdai@gmail.com>
2019-07-09 13:01:55 +0900

    Update movingSolorSystem.py
    add rotate.

--- 3-skyfield/movingSolorSystem.py
+++ 3-skyfield/movingSolorSystem.py
@@ -5,8 +5,67 @@ from matplotlib.animation import FuncAnimation
 import math
 from skyfield.api import load
 from mpl_toolkits.mplot3d import Axes3D
-import mpl_toolkits.mplot3d.art3d as art3d
+from mpl_toolkits.mplot3d import art3d
+from mpl_toolkits.mplot3d import proj3d
 from matplotlib.patches import Circle
+
+def rotation_matrix(v1,v2):
+    """
+    Calculates the rotation matrix that changes v1 into v2.
+    """
+    v1/=np.linalg.norm(v1)
+    v2/=np.linalg.norm(v2)
+
+    cos_angle=np.dot(v1,v2)
+    d=np.cross(v1,v2)
+    sin_angle=np.linalg.norm(d)
+
+    if sin_angle == 0:
+        M = np.identity(3) if cos_angle>0. else -np.identity(3)
+    else:
+        d/=sin_angle
+
+        eye = np.eye(3)
+        ddt = np.outer(d, d)
+        skew = np.array([[    0,  d[2],  -d[1]],
+                      [-d[2],     0,  d[0]],
+                      [d[1], -d[0],    0]], dtype=np.float64)
+
+        M = ddt + cos_angle * (eye - ddt) + sin_angle * skew
+
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z = 0, normal = 'z'):
+    """
+    Transforms a 2D Patch to a 3D patch using the given normal vector.
+
+    The patch is projected into they XY plane, rotated about the origin
+    and finally translated by z.
+    """
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1,0,0), index)
+
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color
+
+    verts = path.vertices #Get the vertices in 2D
+
+    M = rotation_matrix(normal,(0, 0, 1)) #Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    """
+    Translates the 3D pathpatch by the amount delta.
+    """
+    pathpatch._segment3d += delta
 
 def sincos(planets, p1, p2, t):
     pla1, pla2 = planets[p1], planets[p2]
@@ -35,22 +94,22 @@ def createMap(t):
     ret.append(sincos(planets, 'sun', 'venus', t))
     ret.append(sincos(planets, 'sun', 'earth', t))
     ret.append(sincos(planets, 'sun', 'mars', t))
-    #ret.append(sincos(planets, 'sun', 'JUPITER BARYCENTER', t))
-    #ret.append(sincos(planets, 'sun', 'SATURN BARYCENTER', t))
+    ret.append(sincos(planets, 'sun', 'JUPITER BARYCENTER', t))
+    ret.append(sincos(planets, 'sun', 'SATURN BARYCENTER', t))
     #ret.append(sincos(planets, 'sun', 'URANUS BARYCENTER', t))
     #ret.append(sincos(planets, 'sun', 'NEPTUNE BARYCENTER', t))
     x = [x[0] for x in ret]
     y = [x[1] for x in ret]
     z = [x[2] for x in ret]
     r = [x[3] for x in ret]
-    c = ['red', 'blue', 'yellow', 'cyan', 'magenta', 'green', 'green']
+    c = ['red', 'blue', 'yellow', 'cyan', 'magenta', 'green', 'brown']
     c = c[:len(x)]
     return x, y, z, r, c
 
-fig = plt.figure(figsize=(7, 7))
+fig = plt.figure(figsize=(12, 12))
 ax = fig.add_subplot(111, projection='3d')
 #ax.legend()
-hosei = 1.5
+hosei = 5
 ax.set_xlim(-1.0 * hosei, hosei)
 ax.set_ylim(-1.0 * hosei, hosei)
 ax.set_zlim(-1.0 * hosei, hosei)
@@ -58,12 +117,15 @@ ts = load.timescale()
 t = ts.now()
 x, y, z, r, c = createMap(t)
 scat = ax.scatter(x, y, z, c=c)
-scat.set_sizes([40]*len(x))
+scat.set_sizes([50]*len(x))
 i = 0
+rote=((0,0,0),(0,-0.5,0.9),(0,-0.4,0.9),(0,-0.4,0.9),(0,-0.4,0.9),(0,-0.35,0.9),(0,-0.35,0.9))
 for rr in r:
-    q=Circle((0, 0), rr, ec=c[i], fill=False)
-    ax.add_patch(q)
-    art3d.pathpatch_2d_to_3d(q, z=0, zdir="z")
+    p=Circle((0, 0), rr*1.1, ec=c[i], fill=False)
+    ax.add_patch(p)
+    #art3d.pathpatch_2d_to_3d(q, z=0, zdir="z")
+    pathpatch_2d_to_3d(p, z = 0, normal = rote[i])
+    #pathpatch_translate(p, 0.5)
     i += 1
 
 def update(frame_number):
@@ -74,7 +136,7 @@ def update(frame_number):
     t = ts.tt_jd(ts.now().tt+frame_number)
     x, y, z, r, c = createMap(t)
     scat = ax.scatter(x, y, z, c=c)
-    scat.set_sizes([36]*len(x))
+    scat.set_sizes([50]*len(x))
 
 animation = FuncAnimation(fig, update, interval=100)
 plt.show()
