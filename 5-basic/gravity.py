4e2e3559bfcca1cb6810e109847ebca584fa26b0
namickey <teethdai@gmail.com>
2019-07-23 12:59:28 +0900

    Update gravity.py

--- 5-basic/gravity.py
+++ 5-basic/gravity.py
@@ -1,9 +1,28 @@
 #coding:utf-8
 import numpy as np
 
+#地球の重さ
+Me = 5.972*10**24
 #万有引力定数
 G = 6.67259*10**-11 # m**3/s**2/kg**1
-#日心重力定数
-GMs = 1.327*10**20 # m**3/s**2
+print('G*Me:{:20}'.format(G*Me))
+
 #地心重力定数
 GMe = 3.986*10**14 # m**3/s**2
+print('GMe:{:21}'.format(GMe))
+
+#日心重力定数
+GMs = 1.327*10**20 # m**3/s**2
+
+# 「m3 →　km3」では、1000を3回掛ける。
+μ = 3.986*10**14
+print('μ:{:22}'.format(μ))
+print('μk:{:21}'.format(μ/1000/1000/1000))
+μk = 398600 #[km３/s２]
+μk = 3.986*10**5 #[km３/s２]
+print('μk:{:21}'.format(μk))
+
+day = 60*60*24
+au = 1.496*10**8
+print('μau:{:25}'.format(μk/au/au/au))
+print('μauday:{:22}'.format(μk/au/au/au*day*day))
